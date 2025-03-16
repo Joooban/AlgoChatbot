7737029b180e810f29d6c3dfc8c93b2fd0961f0c
Joooban <139736335+Joooban@users.noreply.github.com>
2025-03-16 20:45:59 +0800

    Update Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -2,8 +2,8 @@ import Backend as BE
 
 def main():
     # Streamlit page setup
-    BE.st.set_page_config(page_title="FactUBot", page_icon="🤖")
-    BE.st.title("FactUBot: AI-Powered Fake News Detector 🤖")
+    BE.st.set_page_config(page_title="TruthBot", page_icon="🤖")
+    BE.st.title("TruthBot: AI-Powered Fake News Detector 🤖")
 
     dataset_path = "WELFake_Dataset.csv"  # Path to dataset
     BE.handle_conversation(dataset_path)  # Call the backend function
